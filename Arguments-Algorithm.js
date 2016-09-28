de9caf8ea77398df8c414e38ede3db8976bb1474
elimcjah <Elijah@fungry.com>
2016-09-28 00:03:14 -0600

    Updated the question with the solution

--- Arguments-Algorithm.js
+++ Arguments-Algorithm.js
@@ -1,3 +1,7 @@
+//You will be provided with an initial array (the first argument in the destroyer function), 
+//followed by one or more arguments. Remove all elements from the initial array that are of 
+//the same value as these arguments.
+
 
 function destroyer(arr) {
   // Remove all the values
@@ -5,3 +9,26 @@ function destroyer(arr) {
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+
+
+//SOLUTION:
+
+
+function destroyer(arr) {
+   
+  var args = Array.prototype.slice.call(arguments);
+  return arr.filter(function(x) {
+    
+    return args.indexOf(x) == -1;
+  });
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+//  NOTES:
+//  arguments are SINGLE VALUE OBJECTS a little different from arrays 
+//  in the way that they cannot perform functions like arrays.
+//  For this problem, know that the first array will be indexed array
+//  in which the single value arguments are filtered by.  
+
