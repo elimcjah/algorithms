703f55d90735f5d11042d8f7eafe063f0fc55320
elimcjah <Elijah@fungry.com>
2016-10-01 22:36:47 -0600

    slice(), .length

---
+++ slasher-flick.js
@@ -0,0 +1,19 @@
+// Return the remaining elements of an array after chopping off n elements from the head.
+// The head means the beginning of the array, or the zeroth index.
+
+// PROBLEM:
+
+function slasher(arr, howMany) {
+  // it doesn't always pay to be first
+  return arr;
+}
+
+slasher([1, 2, 3], 2);
+
+//SOLUTION:
+function slasher(arr, howMany) {            
+
+  return arr.slice(howMany, arr.length);    
+}
+
+slasher([1, 2, 3], 2);
