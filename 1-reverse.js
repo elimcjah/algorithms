cc1a1ef88ded5614ee12883a1aebef4ddd981a7b
elimcjah <Elijah@fungry.com>
2016-10-04 22:23:14 -0600

    1 - Reverse a String.   split();, reverse();, join()

---
+++ 1-reverse.js
@@ -0,0 +1,16 @@
+//  Reverse the provided string.
+
+//  PROBLEM:
+
+function reverseString(str) {
+  return str;
+}
+
+
+//  Solution:
+
+function reverseString(str) {
+  return str.split("").reverse().join("");  // Split the String into an array. Reverse the order.
+}                                           // Join the reversed array back into a string. 
+                                            
+reverseString("hello");
