c9440ad5ecce224f709fc4ad8dbdb6add12ae295
elimcjah <Elijah@fungry.com>
2016-10-04 22:11:01 -0600

    14 - Basic Algorithms - Falsy Bouncer

--- boolean.js
+++ boolean.js
@@ -2,7 +2,6 @@
 // Falsy values in JavaScript are false, 
 // null, 0, "", undefined, and NaN.
 
-
 // Problem:
 
 function bouncer(arr) {
@@ -15,6 +14,6 @@ bouncer([7, "ate", "", false, 9]);
 
 function bouncer(arr) {           // Filter the array with the Boolean object. The Boolean
  return arr.filter(Boolean);      // object returns false for all omitted values or values
-}                                 // 0, -0, null, false, NaN, undefined, or the empty string ("")
+}                                 // 0, -0, null, false, NaN, undefined, or the empty string ("").
                                   
 bouncer([7, "ate", "", false, 9]);
