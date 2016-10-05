11b1fbd75112b7ac7779ead9e8d99d27f708ab87
elimcjah <Elijah@fungry.com>
2016-10-04 22:35:30 -0600

    2 - Factorialize a Number. If...else.

---
+++ 2-loops.js
@@ -0,0 +1,31 @@
+// Return the factorial of the provided integer. If the integer is represented 
+// with the letter n, a factorial is the product of all positive integers less 
+// than or equal to n. Factorials are often represented with the shorthand 
+// notation n!
+
+// factorialize(5) should return a number.
+// factorialize(5) should return 120.
+// factorialize(10) should return 3628800.
+// factorialize(20) should return 2432902008176640000.
+// factorialize(0) should return 1.
+
+
+// PROBLEM:
+
+function factorialize(num) {
+return num;
+}
+
+factorialize(3);
+
+
+// SOLUTION:
+
+function factorialize(num) {
+ if(num === 0){                         // If the number is equal to 0
+   return 1;                            // Return 1 to the console.
+ } 
+ return num * (factorialize(num-1));    // Return the factorial of input.
+}
+
+factorialize(3);
