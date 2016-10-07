4f8aac0547c06ddc6f52a73dfb811193e1d82346
elimcjah <Elijah@fungry.com>
2016-10-06 23:44:56 -0600

    06 - Return Largest Numbers in Array. Math.max.();

---
+++ 06-math-max.js
@@ -0,0 +1,32 @@
+// Return an array consisting of the largest number from each provided sub-array. 
+// For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+// largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) 
+// should return an array.
+// largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]) 
+// should return [27,5,39,1001].
+// largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]])
+// should return [9, 35, 97, 1000000].
+
+
+// PROBLEM:
+
+function largestOfFour(arr) {
+  return arr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+// SOLUTION:
+
+function largestOfFour(arr) {
+  var largest=[0,0,0,0];
+  
+  for(i=4;i>0;i--){
+    largest[i-1] = Math.max.apply(Math, arr[i-1]);
+    
+  }
+  return largest;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
