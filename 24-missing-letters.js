7debf28b720125fe492b643b2d2d5b47860ef781
Elijah M <Elijah@fungry.com>
2016-10-14 22:40:06 -0600

    24 - Missing letters. charCodeAt(). fromCharCode()

--- 24-missing-letters.js
+++ 24-missing-letters.js
@@ -16,3 +16,18 @@ fearNotLetter("abce");
 
 // SOLUTION:
 
+function fearNotLetter(str) {
+  
+  var alphabet =  "abcdefghijklmnopqrstuvwxyz";
+  
+  for(i=0;i<alphabet.length; i++){
+    if (alphabet.charCodeAt(i) !== str.charCodeAt(i)){
+     if(i>0)
+       return String.fromCharCode(alphabet.charCodeAt(i));
+     if(i<1)
+       return undefined;  
+    } 
+  }  
+}
+
+fearNotLetter("bcd");
