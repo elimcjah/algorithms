438c8b07fcd59472e3706071e55baf574d7aaecb
Elijah M <Elijah@fungry.com>
2016-10-14 22:27:43 -0600

    24 - Missing letters

---
+++ 24-missing-letters.js
@@ -0,0 +1,18 @@
+// Find the missing letter in the passed letter range and return it.
+// If all letters are present in the range, return undefined.
+
+// fearNotLetter("abce") should return "d".
+// fearNotLetter("abcdefghjklmno") should return "i".
+// fearNotLetter("bcd") should return undefined.
+// fearNotLetter("yz") should return undefined.
+
+// PROBLEM:
+
+function fearNotLetter(str) {
+  return str;
+}
+fearNotLetter("abce");
+
+
+// SOLUTION:
+
