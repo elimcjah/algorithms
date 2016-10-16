30ec787d80c685a4282b5cc9c82282e5325d634a
Elijah M <Elijah@fungry.com>
2016-10-15 22:57:31 -0600

    25 - Boo Who - bool

--- 25-boohoo.js
+++ 25-boohoo.js
@@ -1,13 +1,22 @@
 // PROBLEM:
 
 
-function convertHTML(str) {
-  return str;
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  return bool;
 }
 
-convertHTML("Dolce & Gabbana");
-
+booWho(null);
 
 // SOLUTION:
 
 
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  
+  if(bool === true || bool === false){ 
+    return true;
+  } else return false;
+  }
+
+booWho(true);
