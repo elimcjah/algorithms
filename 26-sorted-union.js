5643453e5761e75a7e64e0e7ab4d0a44ff1197c5
Elijah M <Elijah@fungry.com>
2016-10-15 23:01:30 -0600

    26 - Sorted Union. slice(), concat(), indexOf()

--- 26-sorted-union.js
+++ 26-sorted-union.js
@@ -4,3 +4,21 @@ function uniteUnique(arr) {
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+// SOLUTION:
+
+function uniteUnique(arr) {
+  var newArr = [];
+  var args   = Array.prototype.slice.call(arguments,0);
+  var merged = args.concat.apply([], args);
+   
+    for(var i in merged){
+        if(newArr.indexOf(merged[i]) === -1){  
+          //return newArr;
+          newArr.push(merged[i]);
+        } 
+    }
+  return newArr;      
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
