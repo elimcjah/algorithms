86380c599ad716443d8964f32c1a3f8e24a94ac0
Elijah M <Elijah@fungry.com>
2016-10-17 21:16:06 -0600

    30 - Sum All Primes. Nothing really new. Just putting al the pieces together.

--- 30-primes.js
+++ 30-primes.js
@@ -1,3 +1,13 @@
+// Sum all the prime numbers up to and including the provided number. A prime number is 
+// defined as a number greater than one and having only two divisors, one and itself. For 
+// example, 2 is a prime number because it's only divisible by one and two. The provided 
+// number may not be a prime.
+
+// sumPrimes(10) should return a number.
+// sumPrimes(10) should return 17.
+// sumPrimes(977) should return 73156.
+
+
 // PROBLEM:
 
 
