9ce7353295462f544a36df5fea69748230eb5b02
Elijah M <Elijah@fungry.com>
2016-10-18 23:14:40 -0600

    31 - Max Min Etc

---
+++ 31-max-min.js
@@ -0,0 +1,23 @@
+// new file
+// Find the smallest common multiple of the provided parameters that can be evenly divided by both, 
+// as well as by all sequential numbers in the range between these parameters. The range will be an 
+// array of two numbers that will not necessarily be in numerical order. e.g. for 1 and 3 - find the 
+// smallest common multiple of both 1 and 3 that is evenly divisible by all numbers between 1 and 3.
+
+// smallestCommons([1, 5]) should return a number.
+// smallestCommons([1, 5]) should return 60.
+// smallestCommons([5, 1]) should return 60.
+// smallestCommons([1, 13]) should return 360360.
+// smallestCommons([23, 18]) should return 6056820.
+
+// PROBLEM:
+
+function smallestCommons(arr) {
+ return num;
+}
+
+
+smallestCommons([23,18]);
+
+
+// SOLUTION:
