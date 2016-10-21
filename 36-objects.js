f7c4eb4370f1dd2e862416fb527e18826ba34276
Elijah M <Elijah@fungry.com>
2016-10-20 22:21:43 -0600

    36-objects.js

--- 36-objects.js
+++ 36-objects.js
@@ -1,10 +1,10 @@
-
+// Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+// Remember, you can access object properties through either dot notation or [] notation.
 
 // PROBLEM:
 
 function truthCheck(collection, pre) {
-
-   
+ 
 }
 
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
