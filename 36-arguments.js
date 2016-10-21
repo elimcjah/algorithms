9dd727bc58d2bbce9bbe1b8277681ed8a34a1892
Elijah M <Elijah@fungry.com>
2016-10-20 22:13:42 -0600

    36 - Arguments Optional

---
+++ 36-arguments.js
@@ -0,0 +1,50 @@
+// Create a function that sums two arguments together. If only one argument is provided, then 
+// return a function that expects one argument and returns the sum.  For example, addTogether(2, 3) 
+// should return 5, and addTogether(2) should return a function. Calling this returned function with
+// a single argument will then return the sum:
+// var sumTwoAnd = addTogether(2);
+// sumTwoAnd(3) returns 5.
+// If either argument isn't a valid number, return undefined.
+
+
+// PROBLEM:
+
+
+function addTogether() {
+ return false; 
+}
+
+addTogether("http://bit.ly/IqT6zt");
+
+
+// SOLUTION:
+function addTogether() {
+  
+  var args = [...arguments];
+  
+  
+  
+  //return args.length;
+ // if(args.length === 1){    
+    
+    
+    
+    return function(y){
+      if(!Number.isInteger(y)){
+        return undefined;
+      }
+      return args[0]+y;
+    };   
+    
+  }
+  if(args.length === 2){
+     return args[0]+args[1];
+  }
+ 
+  return undefined;
+  
+}
+
+
+
+addTogether("http://bit.ly/IqT6zt");
