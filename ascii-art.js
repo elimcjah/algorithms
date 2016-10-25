e01eac235393033afdd24554788ddc19b82ce06b
Elijah M <Elijah@fungry.com>
2016-10-24 23:26:57 -0600

    Codingame - ASCII Art Problem

--- ascii-art.js
+++ ascii-art.js
@@ -1,1 +1,21 @@
 // The Codingame ASCII Art Problem
+
+// PROBLEM:
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+var L = parseInt(readline());
+var H = parseInt(readline());
+var T = readline();
+for (var i = 0; i < H; i++) {
+    var ROW = readline();
+}
+
+// Write an action using print()
+// To debug: printErr('Debug messages...');
+
+
+// SOLUTION:
+
