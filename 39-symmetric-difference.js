6d0ef89a35142644937bb64ba48395dd229d3471
Elijah M <Elijah@fungry.com>
2016-10-27 23:49:50 -0600

    39 - Symmetric Difference

--- 39-symmetric-difference.js
+++ 39-symmetric-difference.js
@@ -4,6 +4,16 @@
 // difference you take (say on a set D = {2, 3}), you should get the set with elements which are in either of the two the sets but 
 // not both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}).
 
+// sym([1, 2, 3], [5, 2, 1, 4]) should return [3, 4, 5].
+// sym([1, 2, 3], [5, 2, 1, 4]) should contain only three elements.
+// sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should return [1, 4, 5]
+// sym([1, 2, 5], [2, 3, 5], [3, 4, 5]) should contain only three elements.
+// sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should return [1, 4, 5].
+// sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]) should contain only three elements.
+// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should return [2, 3, 4, 6, 7].
+// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3]) should contain only five elements.
+// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should return [1, 2, 4, 5, 6, 7, 8, 9].
+// sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]) should contain only eight elements.
 
 // PROBLEM:
 
