f4c37c089c5136dc96649c41bdc055cb8bd071ac
Elijah M <Elijah@fungry.com>
2016-10-29 22:50:37 -0600

    42 - No Repeats Please.

--- 42-permutations.js
+++ 42-permutations.js
@@ -3,6 +3,18 @@
 // For example, aab should return 2 because it has 6 total permutations (aab, aab, aba, aba, baa, 
 // baa), but only 2 of them (aba and aba) don't have the same letter (in this case a) repeating.
 
+// permAlone("aab") should return a number.
+// permAlone("aab") should return 2.
+// permAlone("aaa") should return 0.
+// permAlone("aabb") should return 8.
+// permAlone("abcdefa") should return 3600.
+// permAlone("abfdefa") should return 2640.
+// permAlone("zzzzzzzz") should return 0.
+// permAlone("a") should return 1.
+// permAlone("aaab") should return 0.
+// permAlone("aaabb") should return 12.
+
+
 // PROBLEM:
 
 function permAlone(str) {
