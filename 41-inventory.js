3ea23243665b5f0bc6ebfdbd710fffc42ed456bc
Elijah M <Elijah@fungry.com>
2016-10-29 22:42:22 -0600

    41 - Inventory Update.

--- 41-inventory.js
+++ 41-inventory.js
@@ -47,24 +47,26 @@ updateInventory(curInv, newInv);
 
 
 function updateInventory(arr1, arr2) {
-    // All inventory must be accounted for or you're fired!
-    return arr1;
+ 
+  // Loop each array in 2nd argument through the 1st argument. If item exists,
+  // sum up total. Then remove that array from 2nd argument.
+  for(i=0;i<arr2.length;i++){
+    for(j=0;j<arr1.length;j++){
+      if(arr2[i][1] === arr1[j][1]){
+        arr1[j][0] = arr1[j][0] + arr2[i][0];
+        arr2.splice(i,1);
+      } 
+    }
+  }
+  // We currently have duplicate items summed and array of non-duplicates. 
+  // Concat the 2 arrays, sort alphabeticaly by the 2nd item in array and return
+  arr1 = arr1.concat(arr2).sort(function(a,b){
+   return a[1] > b[1];
+  });
+  return arr1;
 }
 
-// Example inventory lists
-var curInv = [
-    [21, "Bowling Ball"],
-    [2, "Dirty Sock"],
-    [1, "Hair Pin"],
-    [5, "Microphone"]
-];
-
-var newInv = [
-    [2, "Hair Pin"],
-    [3, "Half-Eaten Apple"],
-    [67, "Bowling Ball"],
-    [7, "Toothpaste"]
-];
-
-updateInventory(curInv, newInv);
-
+updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], 
+                 [1, "Hair Pin"], [5, "Microphone"]], 
+                [[2, "Hair Pin"], [3, "Half-Eaten Apple"],
+                 [67, "Bowling Ball"], [7, "Toothpaste"]]);
