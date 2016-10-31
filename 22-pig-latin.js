9068089e22be74e117c4c2a853eaf28e1e343ce4
Elijah M <Elijah@fungry.com>
2016-10-30 22:11:20 -0600

    22 - Pig Latin.

--- 22-pig-latin.js
+++ 22-pig-latin.js
@@ -4,6 +4,11 @@
 // If a word begins with a vowel you just add "way" to the end.
 // Input strings are guaranteed to be English words in all lowercase.
 
+// translatePigLatin("california") should return "aliforniacay".
+// translatePigLatin("paragraphs") should return "aragraphspay".
+// translatePigLatin("glove") should return "oveglay".
+// translatePigLatin("algorithm") should return "algorithmway".
+// translatePigLatin("eight") should return "eightway".
 
 // PROBLEM:
 
