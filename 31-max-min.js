38c42770f332c9e5ab7ee972b251efbef2f7d32f
Elijah M <Elijah@fungry.com>
2016-10-31 15:50:56 -0600

    31 - Smallest Common Multiple

--- 31-max-min.js
+++ 31-max-min.js
@@ -21,5 +21,46 @@ smallestCommons([23,18]);
 
 // SOLUTION:
 
+// NOT FINISHED
+function smallestCommons(arr) {
+  
+  var arr2 = [];
+  var c = Math.max(arr[0], arr[1]);
+  var d = Math.min(arr[0], arr[1]); 
+  for(i=0; i <= (c- d); i++){ 
+      arr2[i] =  d + i; 
+  }
+  
+  var arr3 = [];
+  arr3.push(arr2[0], arr2[1]);
+  
+  if(arr2.length>1){
+  return gcd(arr3);
+  }
+  if(arr2.length == 1){
+    return arr2[0];
+  }
+  
+  return arr2;
+  function gcd(arr3){
+    var a = Math.max(arr3[0], arr3[1]);
+    var b = Math.min(arr3[0], arr3[1]); 
+   //return arr3;
+    if(a > b){
+      return gcd([a-b, b]);
+    } 
+    
+    //return arr2;
+    
+    if (a === b){
+      arr2.push(a);
+      return arr2;
+      return smallestCommons(arr2);
+    }
+    
+  }
+  
+}
 
-// Put solution here.
+
+smallestCommons([23,18]);
