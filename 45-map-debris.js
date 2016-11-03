dafd19ea307bb3487ea88584a5f292c8d75ecf14
Elijah M <Elijah@fungry.com>
2016-11-02 23:43:22 -0600

    45 - Map the Debris

--- 45-map-debris.js
+++ 45-map-debris.js
@@ -1,1 +1,13 @@
 //code
+
+
+// PROBLEM:
+
+
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  return arr;
+}
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
