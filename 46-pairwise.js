b055557130258915639abf3495363f5419de68cc
Elijah M <Elijah@fungry.com>
2016-11-04 23:51:21 -0600

    46 - Pairwise

--- 46-pairwise.js
+++ 46-pairwise.js
@@ -12,6 +12,12 @@
 // 9 + 11 = 20 → Indices 1 + 2 = 3
 // 3 + 3 = 6 → Return 6
 
+// pairwise([1, 4, 2, 3, 0, 5], 7) should return 11.
+// pairwise([1, 3, 2, 4], 4) should return 1.
+// pairwise([1, 1, 1], 2) should return 1.
+// pairwise([0, 0, 0, 0, 1, 1], 1) should return 10.
+// pairwise([], 100) should return 0.
+
 // PROBLEM:
 
 function pairwise(arr, arg) {
